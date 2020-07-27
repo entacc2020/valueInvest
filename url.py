a16cba452990012fb1b44032a79a7a2b6340392d
entacc2020 <68633487+entacc2020@users.noreply.github.com>
2020-07-27 17:40:18 +0530

    Update url.py

--- url.py
+++ url.py
@@ -1,3 +1,12 @@
+NIFTYBANKLINK = "https://trendlyne.com/equity/1898/NIFTYBANK/nifty-bank/"
+NIFTYITLINK = "https://trendlyne.com/equity/1902/NIFTYIT/nifty-it/"
+NIFTY50LINK = "https://trendlyne.com/equity/1887/NIFTY50/nifty-50/"
+NIFTYNEXT50 = "https://trendlyne.com/equity/1888/NIFTYNEXT50/nifty-next-50/"
+FILENAME = "output.txt"
 scrneer = "https://www.screener.in"
 morningstar = "https://www.morningstar.in/default.aspx"
-company = "itc ltd"
+company = "bata india"
+
+ 
+
+
